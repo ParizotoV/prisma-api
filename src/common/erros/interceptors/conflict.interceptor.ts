e6fe8ef2f35a2a8d5054bbfae64dee9bdf7b60f0
Vinicius Parizoto <vinicius@clientbase.com.br>
2023-04-11 15:49:36 -0300

    Conflict Interceptor

--- src/common/erros/interceptors/conflict.interceptor.ts
+++ src/common/erros/interceptors/conflict.interceptor.ts
@@ -3,16 +3,18 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  ConflictException,
 } from '@nestjs/common';
 import { Observable, catchError } from 'rxjs';
-import { isPrismaError } from '../utils/is-prisma-error.util';
+import { ConflictError } from '../types/ConflictError';
 
 @Injectable()
-export class DatabaseInterceptor implements NestInterceptor {
+export class ConflitInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
-        if (isPrismaError(error)) {
+        if (error instanceof ConflictError) {
+          throw new ConflictException(error.message);
         } else {
           throw error;
         }
