d4d2b9ffb41676521a019bb88b86729b429f9eee
Vinicius Parizoto <vinicius@clientbase.com.br>
2023-04-11 16:04:01 -0300

    Database Interceptor - concluindo

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { UnauthorizedInterceptor } from './common/erros/interceptors/unauthorized.interceptor';
 import { NotFoundInterceptor } from './common/erros/interceptors/notfound.interceptor';
+import { ConflitInterceptor } from './common/erros/interceptors/conflict.interceptor';
+import { DatabaseInterceptor } from './common/erros/interceptors/database.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +16,8 @@ async function bootstrap() {
     }),
   );
   // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ConflitInterceptor());
+  app.useGlobalInterceptors(new DatabaseInterceptor());
   app.useGlobalInterceptors(new UnauthorizedInterceptor());
   app.useGlobalInterceptors(new NotFoundInterceptor());
   await app.listen(process.env.PORT || 3000);
