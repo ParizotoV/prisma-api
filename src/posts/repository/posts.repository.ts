804aaef161f031f6ac52d2fcd6ef4886e28a930e
Vinicius Parizoto <vinicius@clientbase.com.br>
2023-04-13 09:26:02 -0300

    Relacionamentos com Prisma - parte 2

--- src/posts/repository/posts.repository.ts
+++ src/posts/repository/posts.repository.ts
@@ -3,29 +3,62 @@ import { Post } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from '../dto/create-post.dto';
 import { UpdatePostDto } from '../dto/update-post.dto';
+import { Prisma } from '@prisma/client';
+import { NotFoundError } from 'src/common/erros/types/NotFoundError';
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   public async create(createPostDto: CreatePostDto): Promise<Post> {
+    const { authorEmail } = createPostDto;
+
+    delete createPostDto.authorEmail;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email: authorEmail },
+    });
+
+    if (!user) {
+      throw new NotFoundError('User not found.');
+    }
+
+    const data: Prisma.PostCreateInput = {
+      ...createPostDto,
+      author: { connect: { email: authorEmail } },
+    };
+
     const post = await this.prisma.post.create({
-      data: createPostDto,
+      data,
     });
 
     return post;
   }
 
   public async findAll(): Promise<Post[]> {
-    const posts = await this.prisma.post.findMany();
-
-    return posts;
+    return this.prisma.post.findMany({
+      include: {
+        author: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
   }
 
   public async findOne(id: number): Promise<Post> {
     const post = await this.prisma.post.findUnique({
       where: {
         id,
+      },
+      include: {
+        author: {
+          select: {
+            name: true,
+            email: true,
+          },
+        },
       },
     });
 
