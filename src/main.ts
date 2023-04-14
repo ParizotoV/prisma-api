9d54d152d2ef0da2122962b31a0e3672c679a832
Vinicius Parizoto <vinicius@clientbase.com.br>
2023-04-14 12:23:31 -0300

    Instalação do Swagger no NestJS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { UnauthorizedInterceptor } from './common/erros/interceptors/unauthorize
 import { NotFoundInterceptor } from './common/erros/interceptors/notfound.interceptor';
 import { ConflitInterceptor } from './common/erros/interceptors/conflict.interceptor';
 import { DatabaseInterceptor } from './common/erros/interceptors/database.interceptor';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,14 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Simple blog')
+    .setDescription('The Simple Blog API Description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   // app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ConflitInterceptor());
   app.useGlobalInterceptors(new DatabaseInterceptor());
